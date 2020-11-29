7d9888477dbcc26872d00c3545a8cb3df0802f02
dunke <pintea.dan988@gmail.com>
2020-11-29 19:33:34 +0100

    Dan's shitty classes

--- src/Model/ColourIT.java
+++ src/Model/ColourIT.java
@@ -1,4 +1,23 @@
 package Model;
 
 public class ColourIT {
+
+private TeamMemberList teamMemberList;
+private ProjectList projectList;
+
+  public ColourIT(TeamMemberList teamMemberList, ProjectList projectList)
+  {
+    this.teamMemberList = teamMemberList;
+    this.projectList = projectList;
+  }
+
+  public TeamMemberList getTeamMemberList()
+  {
+    return teamMemberList;
+  }
+
+  public ProjectList getProjectList()
+  {
+    return projectList;
+  }
 }
