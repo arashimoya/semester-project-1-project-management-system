b758d14a59621c5b0f99f7104df9de695690b154
Alex <jardan553@gmail.com>
2020-12-01 13:24:17 +0100

    I addes TeamMember class

--- src/Model/TeamMember.java
+++ src/Model/TeamMember.java
@@ -1,4 +1,20 @@
 package Model;
 
 public class TeamMember {
+  private int id;
+  private String name;
+
+  public TeamMember(int id, String name){
+    this.id=id;
+    this.name=name;
+  }
+  public int getId(){
+    return id;
+  }
+  public String getName(){
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
 }
